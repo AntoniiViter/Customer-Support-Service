d85b4bf49ed83ced73bd0a3294dfcd62e92be757
Antonii Viter <viterantoniy20@gmail.com>
2024-09-16 01:34:13 +0200

    update docker-compose.yml

--- backend/src/main/java/com/example/customersupport/CustomerSupportServiceApplication.java
+++ backend/src/main/java/com/example/customersupport/CustomerSupportServiceApplication.java
@@ -2,9 +2,7 @@ package com.example.customersupport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
 @SpringBootApplication(scanBasePackages = {"com.example.customersupport"})
 @EnableScheduling  // Enable Spring Scheduling
